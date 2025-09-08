bd3a3726dc93d7a9faeb24ce8ea6461da2dbb982
Zonz-Ly <32699823+Zonz-Ly@users.noreply.github.com>
2025-09-08 15:08:09 +0800

    Update setup.rs
    Signed-off-by: Zonz-Ly <32699823+Zonz-Ly@users.noreply.github.com>

--- src-tauri/src/app/setup.rs
+++ src-tauri/src/app/setup.rs
@@ -2,7 +2,6 @@ use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 use tauri::{
-    menu::{MenuBuilder, MenuItemBuilder},
     tray::TrayIconBuilder,
     AppHandle, Manager,
 };
@@ -14,35 +13,17 @@ pub fn set_system_tray(app: &AppHandle, show_system_tray: bool) -> tauri::Result
         app.remove_tray_by_id("pake-tray");
         return Ok(());
     }
-
-    let hide_app = MenuItemBuilder::with_id("hide_app", "Hide").build(app)?;
-    let show_app = MenuItemBuilder::with_id("show_app", "Show").build(app)?;
-    let quit = MenuItemBuilder::with_id("quit", "Quit").build(app)?;
-
-    let menu = MenuBuilder::new(app)
-        .items(&[&hide_app, &show_app, &quit])
-        .build()?;
-
     app.app_handle().remove_tray_by_id("pake-tray");
 
     let tray = TrayIconBuilder::new()
-        .menu(&menu)
-        .on_menu_event(move |app, event| match event.id().as_ref() {
-            "hide_app" => {
-                if let Some(window) = app.get_webview_window("pake") {
-                    window.minimize().unwrap();
-                }
-            }
-            "show_app" => {
+        // 不再设置 .menu()
+        .on_tray_icon_event(|app, event| {
+            if event.click_type == tauri::tray::ClickType::Left {
                 if let Some(window) = app.get_webview_window("pake") {
                     window.show().unwrap();
+                    window.set_focus().unwrap();
                 }
             }
-            "quit" => {
-                app.save_window_state(StateFlags::all()).unwrap();
-                std::process::exit(0);
-            }
-            _ => (),
         })
         .icon(app.default_window_icon().unwrap().clone())
         .build(app)?;
